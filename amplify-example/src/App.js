6561d081f38c740384be20f323471daecd7f6e43
dustvans <dpe4@students.uwf.edu>
2025-02-18 11:52:08 -0600

    init

---
+++ amplify-example/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My AWS Amplify App</h1>
+      <p>This is a simple React app hosted on AWS Amplify.</p>
+    </div>
+  );
+}

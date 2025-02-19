f3555b934a1760fee15b4914261723d2a1c5923b
dustvans <dpe4@students.uwf.edu>
2025-02-18 20:54:24 -0600

    Changed to amplify

--- amplify-example/src/App.js
+++ amplify-example/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to My App Hosted on the Cloud</h1>
+      <h1>Welcome to My App Hosted on AWS Amplify</h1>
       <p>If you're seeing this, it means your app is up and running! 🚀</p>
     </div>
   );
